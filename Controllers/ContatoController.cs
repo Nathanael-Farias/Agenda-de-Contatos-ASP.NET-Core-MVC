721cd6c3788bb8e496ce2637790a4e1d6f5b8bd2
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-04 03:54:08 -0300

    Atualização

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Abstractions;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using ProjetoMVC_DIO.Context;
 using ProjetoMVC_DIO.Models;
 
@@ -33,13 +29,68 @@ namespace ProjetoMVC_DIO.Controllers
         public IActionResult Criar(Contato contato)
         {
             if (ModelState.IsValid)
-             {
+            {
                 _context.Contatos.Add(contato);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
-             }
+            }
             return View(contato);
-            
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+        
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        [HttpGet]
+        public IActionResult Deletar(int id)
+        {
+            var contato = _context.Contatos.FirstOrDefault(c => c.Id == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return View(contato);
+        }
+
+        [HttpPost]
+        [ActionName("Deletar")]
+        public IActionResult DeletarConfirmado(int id)
+        {
+            var contato = _context.Contatos.FirstOrDefault(c => c.Id == id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contatos.Remove(contato);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index)); 
         }
     }
 }
