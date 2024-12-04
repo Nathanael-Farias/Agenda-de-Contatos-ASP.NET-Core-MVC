e3a7bfc7912fb8d2550efab3ca0769ae289baff5
Nathanael-Farias <nathanaelfarias2006@gmail.com>
2024-12-04 05:06:28 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
-using Microsoft.EntityFrameworkCore; // Certifique-se de importar este namespace
-using ProjetoMVC_DIO.Context; // Certifique-se de usar o namespace do seu contexto
+using Microsoft.EntityFrameworkCore; 
+using ProjetoMVC_DIO.Context; 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AgendaContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   
     app.UseHsts();
 }
 
